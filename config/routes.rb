0edf81c8e57f7f33eb0e589003919116bb8ee4ed
vykes <knrymcleish@gmail.com>
2020-05-10 13:14:49 -0500

    add `:auth` namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :questions
+      namespace :auth do
+        post 'signin', to: 'auth#signin'
+      end
     end
-  end  
+  end
 end

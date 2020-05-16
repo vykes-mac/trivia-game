c54844ae41c9ddb0c570b69b0bf286f64b924ba7
vykes <knrymcleish@gmail.com>
2020-05-15 21:49:37 -0500

    convert datetime to integer

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
   class << self
     def encode(payload, exp = 24.hours.from_now)
-      payload[:exp] = exp
+      payload[:exp] = exp.to_i
       JWT.encode(payload, SECRET_KEY)
     end
 

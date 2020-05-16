b465eb57c6e7e99c3b574a59ba10e18d01034ec9
vykes <knrymcleish@gmail.com>
2020-05-15 20:32:37 -0500

    heroku test

--- app/commands/auth/sign_up_user.rb
+++ app/commands/auth/sign_up_user.rb
@@ -11,7 +11,8 @@ module Auth
 
     def call
       @user = insert_user
-      JsonWebToken.encode(user_id: @user.id.$oid) if @user
+
+      JsonWebToken.encode(user_id: @user.id) if @user
     end
 
     private

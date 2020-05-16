5370989872585b1d8bc84d6c106a57010a928c53
vykes <knrymcleish@gmail.com>
2020-05-15 21:37:02 -0500

    heroku test

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,8 +1,8 @@
 class JsonWebToken
   class << self
     def encode(payload, _exp = 24.hours.from_now)
-      hr = (Time.now + 1.day).to_i
-      payload[:hr] = hr
+      # hr = (Time.now + 1.day).to_i
+      # payload[:hr] = hr
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
